fc825df3113ba802948faa8e8bbe5fbc4aed203b
Andy Lee <freerangeandy@gmail.com>
2020-05-03 20:22:49 -0400

    removed toolbar

--- app/javascript/react/App.js
+++ app/javascript/react/App.js
@@ -17,10 +17,9 @@ export const App = (props) => {
         'searchreplace visualblocks code fullscreen',
         'insertdatetime media table paste code help wordcount'
        ],
-       toolbar:
-        'undo redo | formatselect | bold italic backcolor | \
-        alignleft aligncenter alignright alignjustify | \
-        bullist numlist outdent indent | removeformat | help'
+       toolbar: false,
+       branding: false,
+       maxWidth: 300
       }}
       onEditorChange={this.handleEditorChange}
    />
