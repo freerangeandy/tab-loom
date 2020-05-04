180f68cdc9371f703e358108e5d77c191c303f28
Andy Lee <freerangeandy@gmail.com>
2020-05-03 20:12:44 -0400

    installed (w/api key) tinymce as text editor

--- app/javascript/react/App.js
+++ app/javascript/react/App.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  const handleEditorChange = (content, editor) => {
+    console.log('Content was updated:', content);
+  }
+
+  return (
+    <Editor
+      initialValue="<p>This is the initial content of the editor</p>"
+      init={{
+       height: 500,
+       menubar: false,
+       plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount'
+       ],
+       toolbar:
+        'undo redo | formatselect | bold italic backcolor | \
+        alignleft aligncenter alignright alignjustify | \
+        bullist numlist outdent indent | removeformat | help'
+      }}
+      onEditorChange={this.handleEditorChange}
+   />
+  );
 }
 
 export default App
