6fa2e5fcec86a2d31713e7cd31662a99d98c6bc6
Andy Lee <freerangeandy@gmail.com>
2020-05-07 14:58:45 -0400

    restored containers with correct name spelling

---
+++ app/javascript/react/containers/ChordsSidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar from 'react-sidebar'
+
+import ChordsContent from '../components/ChordsContent'
+
+const ChordsSidebar = props => {
+  return (
+    <Sidebar
+      sidebar={<ChordsContent />}
+      docked={true}
+      pullRight
+      defaultSidebarWidth={'0'}
+      rootClassName={'sidebarRight'}
+    >
+      <div> </div>
+    </Sidebar>
+  )
+}
+
+export default ChordsSidebar
