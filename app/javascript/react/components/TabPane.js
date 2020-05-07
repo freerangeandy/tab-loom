b33372e377913c4fe1be931407ad137d912b50fe
Andy Lee <freerangeandy@gmail.com>
2020-05-07 15:52:03 -0400

    nested tab editor in tab pane component; adjusted container spacing

---
+++ app/javascript/react/components/TabPane.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+
+import TabEditor from '../components/TabEditor'
+
+const TabPane = props => {
+  return (
+    <>
+      <h5> TAB NAME </h5>
+      <TabEditor />
+    </>
+  )
+}
+
+export default TabPane
