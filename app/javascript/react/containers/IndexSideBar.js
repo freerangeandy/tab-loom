474e5453db45d6cdf655ede0c7a4ff81021854e3
Andy Lee <freerangeandy@gmail.com>
2020-05-07 16:28:31 -0400

    add Sidebar containers (to fix spelling)

---
+++ app/javascript/react/containers/IndexSideBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Sidebar from 'react-sidebar'
+
+import IndexContent from '../components/IndexContent'
+
+const IndexSidebar = props => {
+  return (
+    <Sidebar
+      sidebar={<IndexContent />}
+      docked={true}
+      defaultSidebarWidth={0}
+      rootClassName={'sidebarLeft'}
+    >
+      <div> </div>
+    </Sidebar>
+  )
+}
+
+export default IndexSidebar
