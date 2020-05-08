b4850cb0084f095dce4084b67d41f70d6fc02ef1
Andy Lee <freerangeandy@gmail.com>
2020-05-07 23:22:21 -0400

    added examples to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-sample = %Q(
+verse = %Q(
 e|-----------------|----------------|----------------|----------------|
 B|-------1---------|------3---------|------1---------|------3---------|
 G|---0-------0-----|--0-------0-----|--0-------0-----|--0-------0-----|
@@ -7,6 +7,14 @@ A|-3-------3-------|2-------2-------|0-------0-------|2-------2-------|
 E|-----------------|----------------|----------------|----------------|
 )
 
+chorus = %Q(
+e|-------3---------|----------------|-------0--------|-0-----0--------|
+B|---0-------0-----|-1-----1--------|-3--------------|---1------------|
+G|-----------------|---0------------|---0------------|-----0----------|
+D|-----------------|-----2-----2----|-----0-----0----|------2-----2---|
+A|-----2-------2---|-0-------0------|-2-------2------|--0-------0-----|
+E|-3-------3-------|----------------|----------------|----------------|
+)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -15,4 +23,6 @@ E|-----------------|----------------|----------------|----------------|
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 basic_user = User.create(username: 'basic', email: 'basic@bort.com', password: 'bortman', password_confirmation: 'bortman')
-landslide_verse = Tablature.create(title: "Landslide verse", content: sample, user: basic_user)
+another_user = User.create(username: 'another', email: 'another@bort.com', password: 'falafel', password_confirmation: 'falafel')
+landslide_verse = Tablature.create(title: "Landslide verse", content: verse, user: basic_user)
+landslide_chorus = Tablature.create(title: "Landslide chorus", content: chorus, user: another_user)
