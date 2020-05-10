25d1c745a3422def99c294007f6523f1b2edba7c
Andy Lee <freerangeandy@gmail.com>
2020-05-10 14:52:55 -0400

    wrapped app in provider connected to store

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
-
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import rootReducer from '../react/reducers'
+import { Provider } from 'react-redux'
 
 import App from '../react/App'
 import RedBox from 'redbox-react'
@@ -8,16 +10,22 @@ import RedBox from 'redbox-react'
 document.addEventListener('DOMContentLoaded', () => {
   let reactElement = document.getElementById('app')
 
+  const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
+  const appWithProvider = <Provider store={store}><App /></Provider>
   if (reactElement) {
     if(window.railsEnv && window.railsEnv === 'development'){
       try {
-        render(<App />, reactElement)
+        render(appWithProvider, reactElement)
       } catch (e) {
         render(<RedBox error={e} />, reactElement)
       }
     }
     else {
-      render(<App />, reactElement)
+      render(appWithProvider, reactElement)
     }
   }
 })
