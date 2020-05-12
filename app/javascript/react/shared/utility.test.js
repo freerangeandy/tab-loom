ac50519925853acd0cf0d30583c1e158aa7ed2c2
Andy Lee <freerangeandy@gmail.com>
2020-05-11 22:39:35 -0400

    wrote tests for overwrite method; changed some refernces insert->overwrite

--- app/javascript/react/shared/utility.test.js
+++ app/javascript/react/shared/utility.test.js
@@ -1,28 +1,60 @@
 import React from "react"
 import { BLANK_TAB, COLUMN_COUNT } from './inStringConsts'
 import {
+  overtypeStringAtPos,
   getContentAfterChordInsert
 } from './utility.js'
-//
-// describe("example", () => {
-//   it('should not print the error list', () => {
-//     expect(true).toBe(true)
-//   })
-// })
+
+describe("overtypeStringAtPos", () => {
+  let testString, overwriteChar
+
+  beforeEach(() => {
+    testString = "0123456"
+    overwriteChar = "y"
+  })
+
+  it('should not return a string of different length than the input', () => {
+    const overwritePos = 3
+    const stringAfterOverwrite = overtypeStringAtPos(testString, overwritePos, overwriteChar)
+    expect(stringAfterOverwrite.length).toEqual(testString.length)
+  })
+
+  it('should replace the existing char at target position with the overwrite char', () => {
+    const overwritePos = 3
+    const previousCharAtPos = testString.charAt(overwritePos)
+    const stringAfterOverwrite = overtypeStringAtPos(testString, overwritePos, overwriteChar)
+    expect(stringAfterOverwrite.indexOf(previousCharAtPos)).toEqual(-1)
+    expect(stringAfterOverwrite.charAt(overwritePos)).toEqual(overwriteChar)
+  })
+
+  it('should not return a string of different length than the input when overwriting last character', () => {
+    const overwritePos = testString.length - 1
+    const stringAfterOverwrite = overtypeStringAtPos(testString, overwritePos, overwriteChar)
+    expect(stringAfterOverwrite.length).toEqual(testString.length)
+  })
+
+  it('should replace the lat char at target position with the overwrite char', () => {
+    const overwritePos = testString.length - 1
+    const previousCharAtPos = testString.charAt(overwritePos)
+    const stringAfterOverwrite = overtypeStringAtPos(testString, overwritePos, overwriteChar)
+    expect(stringAfterOverwrite.indexOf(previousCharAtPos)).toEqual(-1)
+    expect(stringAfterOverwrite.charAt(overwritePos)).toEqual(overwriteChar)
+  })
+})
 
 describe("getContentAfterChordInsert", () => {
   let testContent, testChordNotes, testColumn, numActiveCols, prefixLength
 
   beforeEach(() => {
     testContent = BLANK_TAB
-    numActiveCols = COLUMN_COUNT - 2
     prefixLength = 2
+    numActiveCols = COLUMN_COUNT - prefixLength
     testChordNotes = [3, 2, 0, 0, 0, 3]
   })
 
   it('should insert a chord into the first column (after row prefix)', () => {
     testColumn = prefixLength + 0
-    const expectedContent =  [
+    const expectedContent = [
       '<p>e|'.concat(testChordNotes[5]).concat('-'.repeat(numActiveCols - 1)).concat('</p>'),
       '<p>B|'.concat(testChordNotes[4]).concat('-'.repeat(numActiveCols - 1)).concat('</p>'),
       '<p>G|'.concat(testChordNotes[3]).concat('-'.repeat(numActiveCols - 1)).concat('</p>'),
