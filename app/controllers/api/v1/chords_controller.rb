d10d12a701ddfbc5f4ae0b3ad500a650dd9c75af
Andy Lee <freerangeandy@gmail.com>
2020-05-12 22:44:16 -0400

    reformatted chord list received from API

--- app/controllers/api/v1/chords_controller.rb
+++ app/controllers/api/v1/chords_controller.rb
@@ -8,7 +8,27 @@ class Api::V1::ChordsController < ApplicationController
     url = "https://api.uberchord.com/v1/chords?names=#{api_params}"
     api_response = Faraday.get(url)
     parsed_response = JSON.parse(api_response.body)
+    reformatted_response = reformat_chord_data(parsed_response)
 
-    render json: parsed_response
+    render json: reformatted_response
+  end
+
+  private
+
+  def reformat_chord_name(chord_name)
+    chord_name_split = chord_name.split(',')
+    if !chord_name.include?(",,,")
+      return "#{chord_name_split[0]}_#{chord_name_split[1..3].join('')}"
+    else
+      return chord_name_split[0]
+    end
+  end
+
+  def reformat_chord_data(chord_list)
+    return reformatted_list = chord_list.map do |chord_obj|
+      chord_strings = chord_obj["strings"].split(" ")
+      chord_name = reformat_chord_name(chord_obj["chordName"])
+      { strings: chord_strings, name: chord_name }
+    end
   end
 end
