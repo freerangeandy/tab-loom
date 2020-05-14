d788f074bad08efed5f3f337be454daec2571a21
Andy Lee <freerangeandy@gmail.com>
2020-05-14 00:55:04 -0400

    added dots, string labels, hover transitions to fretboard

--- app/javascript/react/containers/FretboardContainer.js
+++ app/javascript/react/containers/FretboardContainer.js
@@ -2,13 +2,41 @@ import React, { Fragment } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 import StringOverlay from '../components/StringOverlay'
+import Dot from '../components/Dot'
 
 const FretboardContainer = (props) => {
-  const columns = (numCols) => {
-    const colGroup = Array.from(Array(numCols).keys()).map(val => {
-      return (
-        <Col key={val} xs={2} className="fretArea">
+  const showDot = (string, fret) => {
+    if (string == 3 && fret == 12
+      || string == 5 && fret == 12
+      || string == 4 && fret == 9
+      || string == 4 && fret == 7
+      || string == 4 && fret == 5
+      || string == 3 && fret == 3
+      || string == 5 && fret == 3
+    ) {
+      return true
+    } else return false
+  }
 
+  const clickHandler = (stringNum, fretNum) => (event) => {
+    console.log(`String: ${stringNum}, Fret: ${fretNum}`)
+  }
+
+  const makeRow = (row, numCols, colOffset = 0) => {
+    const colGroup = Array.from(Array(numCols).keys()).map(val => {
+      const stringNum = row + 1
+      const fretNum = val + colOffset + 1
+
+      let dot
+      if (showDot(stringNum, fretNum)) {
+        dot = <Dot />
+      }
+      return (
+        <Col key={val}
+          xs={2}
+          className="fretArea"
+          onClick={clickHandler(stringNum, fretNum)}>
+         {dot}
         </Col>
       )
     })
@@ -16,21 +44,21 @@ const FretboardContainer = (props) => {
     return <Row>{colGroup}</Row>
   }
 
-  const columns1 = columns(6)
-  const columns2 = columns(6)
-  const columns3 = columns(6)
+  const grid = Array.from(Array(6).keys()).map((val, row) => {
+    const fretArea1 = makeRow(row, 6)
+    const fretArea2 = makeRow(row, 6, 6)
+    const fretArea3 = makeRow(row, 6, 12)
 
-  const grid = Array.from(Array(6).keys()).map(val => {
     return (
       <Row className="fretboardRow" key={val}>
         <Col xs={6}>
-          {columns1}
+          {fretArea1}
         </Col>
         <Col xs={4}>
-          {columns2}
+          {fretArea2}
         </Col>
         <Col xs={2}>
-          {columns3}
+          {fretArea3}
         </Col>
       </Row>
     )
@@ -41,6 +69,14 @@ const FretboardContainer = (props) => {
         {grid}
       </Container>
       <StringOverlay />
+      <ul className="stringLabels">
+        <li onClick={clickHandler(1, 0)}>e</li>
+        <li onClick={clickHandler(2, 0)}>B</li>
+        <li onClick={clickHandler(3, 0)}>G</li>
+        <li onClick={clickHandler(4, 0)}>D</li>
+        <li onClick={clickHandler(5, 0)}>A</li>
+        <li onClick={clickHandler(6, 0)}>E</li>
+      </ul>
     </>
   )
 }
