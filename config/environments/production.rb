184fd4bc86929fc3831fcb16fd5fd73b4f36d397
Andy Lee <freerangeandy@gmail.com>
2020-05-15 00:32:22 -0400

    fix static files not precompiling for push to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.serve_static_files = true
+  config.assets.compile = true
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,6 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-config.serve_static_files = true
-config.assets.compile = true
