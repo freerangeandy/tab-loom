9a7bb01c1b873b04cf16f117324b114c73929727
Andy Lee <freerangeandy@gmail.com>
2020-05-15 00:19:21 -0400

    ensured static images would be served to heroku

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+config.serve_static_files = true
+config.assets.compile = true
