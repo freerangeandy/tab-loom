8af6faeabd773427aac19f30c79494a9cebff910
Andy Lee <freerangeandy@gmail.com>
2020-05-14 22:05:30 -0400

    improved hover behavior for index text

--- app/javascript/react/components/IndexContent.js
+++ app/javascript/react/components/IndexContent.js
@@ -51,7 +51,7 @@ const IndexContent = props => {
   let newTabButton = <h5>Sign in to add new tabs</h5>
   if (currentUser.id != null) {
     newTabButton = (
-      <h5 className="index-item" onClick={() => showNewTab()}>+ New Tab</h5>
+      <h5 className="new-tab" onClick={() => showNewTab()}>+ New Tab</h5>
     )
   }
 
