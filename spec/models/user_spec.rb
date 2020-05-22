506e68126f08fffd540b641db944807367576d15
Andy Lee <freerangeandy@gmail.com>
2020-05-22 15:18:08 -0400

    added User model tests for .new and .valid?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:complete_user) { User.new(username: "complete", password: "2complete4u", password_confirmation: "2complete4u", email: "this@isntreal.com") }
+
+  describe ".new" do
+    it "should return a User object" do
+      new_user = build(:user)
+      expect(new_user).to be_a(User)
+    end
+  end
+
+  describe ".valid?" do
+    it "should return true if all fields entered" do
+      expect(complete_user.valid?).to be(true)
+    end
+
+    it "should return false if any fields missing" do
+      no_username = build(:user, username: "")
+      no_email = build(:user, email: "")
+      no_passwords = build(:user, password: "", password_confirmation: "")
+
+      expect(no_username.valid?).to be(false)
+      expect(no_email.valid?).to be(false)
+      expect(no_passwords.valid?).to be(false)
+    end
+  end
 end
