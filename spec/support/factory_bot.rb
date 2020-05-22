172d6cf46f7a97b68302eaa6c0477e8ee5ef51e4
Andy Lee <freerangeandy@gmail.com>
2020-05-22 15:13:16 -0400

    enabled use of FactoryBot methods in tests; varied User factory passwords

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
   factory :user do
     sequence(:username) {|n| "user#{n}" }
     sequence(:email) {|n| "user#{n}@example.com" }
-    password { 'password' }
-    password_confirmation { 'password' }
+    password {|n| "pass#{n}word" }
+    password_confirmation {|n| "pass#{n}word" }
   end
 
 end
