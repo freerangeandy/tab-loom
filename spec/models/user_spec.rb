b2d48993313dd0dfc4bb5fd6b6b105fe37f91108
Andy Lee <freerangeandy@gmail.com>
2020-05-22 16:19:05 -0400

    added User model tests for instance variables

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:complete_user) { User.new(username: "complete", password: "2complete4u", password_confirmation: "2complete4u", email: "this@isntreal.com") }
-
   describe ".new" do
     it "should return a User object" do
       new_user = build(:user)
@@ -12,6 +10,7 @@ RSpec.describe User, type: :model do
 
   describe ".valid?" do
     it "should return true if all fields entered" do
+      complete_user = build(:user)
       expect(complete_user.valid?).to be(true)
     end
 
@@ -23,6 +22,31 @@ RSpec.describe User, type: :model do
       expect(no_username.valid?).to be(false)
       expect(no_email.valid?).to be(false)
       expect(no_passwords.valid?).to be(false)
+    end
+  end
+
+  context "instance variables" do
+    before(:example) do
+      complete_user = User.create(username: "complete", password: "2complete4u", password_confirmation: "2complete4u", email: "this@isntreal.com")
+      @complete_user_saved = User.find(complete_user.id)
+    end
+
+    describe "#username" do
+      it "should return the username" do
+        expect(@complete_user_saved.username).to eq("complete")
+      end
+    end
+
+    describe "#email" do
+      it "should return the email address" do
+        expect(@complete_user_saved.email).to eq("this@isntreal.com")
+      end
+    end
+
+    describe "#password" do
+      it "should not return the actual password" do
+        expect(@complete_user_saved.password).to eq(nil)
+      end
     end
   end
 end
