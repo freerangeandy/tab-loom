0cc8914a8e5cf37abf60192dc60dc8669f34a6dc
Andy Lee <freerangeandy@gmail.com>
2020-05-23 17:38:57 -0400

    added tab controller tests for PATCH update route

--- spec/controllers/api/v1/tablatures_controller_spec.rb
+++ spec/controllers/api/v1/tablatures_controller_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe Api::V1::TablaturesController, type: :controller do
   before(:example) do
     @user = create(:user_with_tabs, username: "current")
+    @first_tab = @user.tablatures.first
+    @prev_count = Tablature.count
     sign_in @user
   end
 
   describe "GET#show" do
-    before(:example) { @first_tab = @user.tablatures.first }
-
     it "returns a successful response status and a content type of JSON" do
       get :show, params: {id: @first_tab.id}
 
@@ -49,7 +49,6 @@ RSpec.describe Api::V1::TablaturesController, type: :controller do
   end
 
   describe "POST#create" do
-    before(:example) { @prev_count = Tablature.count }
     it "posts a new tablature upon successful request" do
       post_json = {
         tablature: {
@@ -145,7 +144,7 @@ RSpec.describe Api::V1::TablaturesController, type: :controller do
         tablature: {
           title: "Not a user",
           content: "--41--42",
-          user_id: -1
+          user_id: 0
         }
       }
 
@@ -160,9 +159,96 @@ RSpec.describe Api::V1::TablaturesController, type: :controller do
     end
   end
 
-  xdescribe "PATCH#update" do
+  describe "PATCH#update" do
+    it "returns a successful response status and a content type of JSON" do
+      patch_json = {
+        id: @first_tab.id,
+        tablature: {
+          title: "Sample Tab",
+          content: "--41--42",
+          user_id: @user.id
+        }
+      }
+      patch :update, params: patch_json
 
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+      expect(Tablature.count).to eq(@prev_count)
+    end
 
+    it "returns the JSON of the updated tablature" do
+      patch_json = {
+        id: @first_tab.id,
+        tablature: {
+          title: "Sample Tab",
+          content: "--41--42",
+          user_id: @user.id
+        }
+      }
+      patch :update, params: patch_json
+
+      returned_json = JSON.parse(response.body)
+      expect(returned_json).to be_kind_of(Hash)
+      expect(returned_json).to_not be_kind_of(Array)
+      expect(returned_json["title"]).to eq(patch_json[:tablature][:title])
+      expect(returned_json["content"]).to eq(patch_json[:tablature][:content])
+      expect(returned_json["user_id"]).to eq(@user.id)
+      expect(returned_json["user_name"]).to eq(@user.username)
+    end
+
+    it "returns errors if title field missing" do
+      missing_title_json = {
+        id: @first_tab.id,
+        tablature: {
+          title: "",
+          content: "--41--42",
+          user_id: @user.id
+        }
+      }
+      patch :update, params: missing_title_json
+
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(returned_json["error"]).to eq "Title can't be blank"
+    end
+
+    it "returns errors if content field missing" do
+      missing_content_json = {
+        id: @first_tab.id,
+        tablature: {
+          title: "Sample Tab",
+          content: "",
+          user_id: @user.id
+        }
+      }
+      patch :update, params: missing_content_json
+
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(returned_json["error"]).to eq "Content can't be blank"
+    end
+
+    it "returns errors if user id missing" do
+      user_wrong_json = {
+        id: @first_tab.id,
+        tablature: {
+          title: "Sample Tab",
+          content: "--41--42",
+          user_id: 0
+        }
+      }
+      patch :update, params: user_wrong_json
+
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(returned_json["error"]).to eq "User must exist and User can't be blank"
+    end
   end
 
   xdescribe "DELETE#destroy" do
