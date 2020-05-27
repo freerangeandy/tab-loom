d49e043852d18c9bca4fc3be43a46943ddfe7b44
Andy Lee <freerangeandy@gmail.com>
2020-05-27 17:46:02 -0400

    added modal which appears on clicking index item or new tab while changes unsaved

--- app/javascript/react/components/IndexContent.js
+++ app/javascript/react/components/IndexContent.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { Modal, Button } from 'react-bootstrap'
 
 import IndexItem from './IndexItem'
 import allActions from '../actions'
 
 const IndexContent = props => {
   const deleteTabByIndex = props.deleteTabByIndex
+  const [clickIndex, setClickIndex] = useState(null)
   const dispatch = useDispatch()
   const currentUser = useSelector(state => state.currentUser)
   const tabList = useSelector(state => state.userTabs.list)
   const tabSelectedIndex = useSelector(state => state.userTabs.selectedIndex)
+  const saveable = useSelector(state => state.tabEditor.saveable)
   const resetColumn = () => {
     dispatch(allActions.editorActions.resetColumn())
   }
@@ -32,6 +35,36 @@ const IndexContent = props => {
     setSaveable(false)
   }
 
+  const handleIndexClick = (index) => {
+    if (saveable) {
+      setClickIndex(index)
+    } else {
+      if (index === -1) {
+        showNewTab()
+      } else {
+        setTabSelectedIndex(index)
+      }
+    }
+  }
+
+  const handleConfirm = () => {
+    if (clickIndex === -1) {
+      showNewTab()
+    } else {
+      setTabSelectedIndex(clickIndex)
+    }
+    setClickIndex(null)
+  }
+
+  const handleCancel = () => {
+    setClickIndex(null)
+  }
+
+  const showModal = () => {
+    if (clickIndex === null) return false
+    else                     return true
+  }
+
   let tabDisplayList
   if (tabList.length > 0) {
     tabDisplayList = tabList.map((tab, index) => {
@@ -43,7 +76,7 @@ const IndexContent = props => {
           indexItemClass={indexItemClass}
           indexItemTitle={tab.title}
           deleteHandler={() => deleteTabByIndex(index)}
-          clickHandler={() => setTabSelectedIndex(index)} />
+          clickHandler={() => handleIndexClick(index)} />
       )
     })
   }
@@ -51,16 +84,32 @@ const IndexContent = props => {
   let newTabButton = (<a href="/users/sign_in"><h5 className="index-item">Sign in to add new tabs</h5></a>)
   if (currentUser.id != null) {
     newTabButton = (
-      <h5 className="new-tab" onClick={() => showNewTab()}>+ New Tab</h5>
+      <h5 className="new-tab" onClick={() => handleIndexClick(-1)}>+ New Tab</h5>
     )
   }
 
   return (
-    <div>
-      <h4>{currentUser.username}</h4>
-      <ul>{tabDisplayList}</ul>
-      {newTabButton}
-    </div>
+    <>
+      <div>
+        <h4>{currentUser.username}</h4>
+        <ul>{tabDisplayList}</ul>
+        {newTabButton}
+      </div>
+      <Modal show={showModal()} onHide={handleCancel}>
+        <Modal.Header closeButton>
+          <Modal.Title>Unsaved changes</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You have unsaved work in the tab editor which will be lost. Do you still want to (view this tab/create a new tab)?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCancel}>
+            Back
+          </Button>
+          <Button variant="primary" onClick={handleConfirm}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 
